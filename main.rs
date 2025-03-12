bf75b90a0823f1bb3ae40a05a8a1160e94b97db5
Myname134 <bhuvan21021@gmail.com>
2025-03-12 12:13:18 +0530

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,33 +1,40 @@
-use std::cmp::Ordering;
-use std::io;
-use rand::Rng;
-use colored::*;
+/*To download rust, just go with installation process in the Book available on Rust offical website. You have to read atleat 
+basic amount of information from the documentaion provided by any language officials as it is a good practice.*/
+
+//Let's start with your first Rust code.
+
+/*To create a rust file, if your using an IDE then create a folder with ".rs" extention. or using a text editor in terminal
+create a new directory for this course and create a file with ".rs"*/
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1,101);
-
-    println!("The secret number is: {}", secret_number);
-
-    loop{
-        println!("Please input your guess.");
-        let mut guess= String::new();
-        io::stdin().read_line(&mut guess).unwrap();
-        let guess: u32 = match guess.trim().parse() {
-	    Ok(num) => num,
-	    Err(_) => continue,
-        };    
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-	    Ordering::Less => println!("{}", "Too small!".purple()),
-	    Ordering::Greater => println!("{}", "Too big!".red()),
-	    Ordering::Equal => {
-                println!("{}", "You win!".yellow());
-                break; 
-            }
-        }
-    }
+    println!("Angelina Jolie");
 }
+
+/*If you atleast don't know how to run a code, I would suggest him/her/them/F&%$$#% to stop coding.
+If you are using anything like VScode or jetbrains etc. Just use RUN button. I myself using a text editor, a easy one which
+is called nano, which is for "gays" ,but anyway in this i will teach you with nano.*/
+
+/* To run in a compiler type "rustc main.rs" and then "rust main". Your code gets executed and and output is shown in below
+line. By the way main is our file name.*/
+
+/*What if after typing the first thing you got something down below it,"JUST GO AND DIE", type the code correctly which is 
+shown you above. And the lines which come under them are called errors and let me tell you rust has the best error correcting
+suggestion,unlike other languages where you can't get a clear cut view of your mistake.*/
+
+/* Let's divide the structure of rust code. 
+"fn main()" this defines a function called main, you can use any like" MY_A####"
+but if your using 2 or more words use a '_' in between you should know this as to divide something in rust use that symbol.
+The function body is in between "{}". It is a good practise to give the opening curly bracket in the same line with a one 
+space between them. "fn main() { }". 
+And the line "println!("A$$$$$$")" is our actual code. println! is a rust macro. We will dive into it deeper later but as of
+now use a '!'. if you don't use the'ln' after 'print', it won't go to next line after excution. 
+Don't ask me what is it (Idiots).
+";" is used to end a line of code like in java.It just says that this line is over and next line is ready to begin.Most of
+the lines in java end with ';'.
+SO YOU DEFINE A FUNCTION WRITE A CODE INSIDE AND EXCUTED IT.*/
+
+And forgot to mention I used comments to write the non code part as it is not executed we use 2 / if it a same line if it is
+multiple lines we use a * and / you can see above and this is difference of writing something in code with comments and 
+without comments but you should not write non code part withour comments like me.  
+
+*Note pay attention on rust lang not on my english lang.
